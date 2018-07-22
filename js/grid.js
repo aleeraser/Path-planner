f7caec400d5314bc525031de1cb56495357e84aa
aleeraser <alezini94@hotmail.it>
2018-07-22 02:46:51 +0200

    Fixed subtle coordinate system bug. Fixed wrong functions use. Started restyling.

--- js/grid.js
+++ js/grid.js
@@ -1,6 +1,6 @@
 class Grid {
 
-    constructor(canvas_id, side_length, cell_number) {
+    constructor(canvas_id, width, height) {
         this.CIRCLE = 'TYPE_CIRCLE';
         this.RECT = 'TYPE_RECT';
         this.LINE = 'TYPE_LINE';
@@ -20,26 +20,29 @@ class Grid {
 
         this.canvas_id = canvas_id;
         this.canvas_obj = document.getElementById(this.canvas_id);
+        this.canvas_obj.setAttribute("width", width);
+        this.canvas_obj.setAttribute("height", height);
         this.context = this.canvas_obj.getContext("2d");
 
         // Canvas size and cell number
-        this.canvas_obj.width = this.canvas_obj.height = side_length;
-        this.size_x = this.size_y = cell_number;
+        // TODO: rendi la dimensione delle celle selezionabile con una combo box da 3 opzioni: piccolo medio grande
+        this.size = {
+            x: -1,
+            y: -1
+        };
 
-        // Pixel distance between grid cells
-        this.spacing_x = 5;
-        this.spacing_y = 5;
+        // Default pixel distance between grid cells
+        this.setSpacing(1, 1);
 
         // Color configurations
         this.WALL_COLOR = "#d5a76b";
-        this.WALL_COLOR = "#d5a76b";
-        this.BG_COLOR = "#142b3f";
+        this.BG_COLOR = "#eeeeee";
         this.START_COLOR = "#00ff00";
         this.END_COLOR = "#ff0000";
         this.LINE_COLOR = "#b18ec5";
         this.PATH_COLOR = "#b18ec5";
         this.BEST_PATH_COLOR = "green";
-        this.CELL_COLOR = this.BG_COLOR;
+        this.CELL_COLOR = "#142b3f";
         this.OBSTACLE_EDGE_COLOR = "#996600";
 
         // Cell size info
@@ -78,55 +81,87 @@ class Grid {
         this.drawPath = false;
     }
 
+    // Generate the grid based on setting specified before
+    generate() {
+        if (this.size.x == null || this.size.y == null) {
+            console.error("Missing size parameters.");
+            return false;
+        }
+
+        var cell_side_lenght = 30;
+
+        var x = Math.ceil(((this.canvas_obj.width - this.spacing_x) / cell_side_lenght) - this.spacing_x);
+        var y = Math.ceil(((this.canvas_obj.height - this.spacing_y) / cell_side_lenght) - this.spacing_y);
+
+        var cell_n = {
+            x: x,
+            y: y
+        };
+        console.log("There can be " + cell_n.x + "x" + cell_n.y + " cells of side 100.");
+
+        this.size = cell_n;
+
+
+        // Calculate size in pixel based on dimensions and number of cells
+        this.cell_width = (this.canvas_obj.width - this.spacing_x) / (cell_n.x + this.spacing_x);
+        // this.cell_width = cell_side_lenght;
+        // this.cell_width = ((this.canvas_obj.width - this.spacing_x) / 100) - this.spacing_x;
+        this.cell_height = (this.canvas_obj.height - this.spacing_y) / (cell_n.y + this.spacing_y);
+        // this.cell_height = cell_side_lenght;
+
+        console.log(this.cell_width, this.cell_height);
+
+        this.grid_matrix = [];
+        this.wall_map = [];
+
+        // for (var i = 0; i < this.size.y; i++) {
+        //     // this.wall_map[i] = [];
+
+        //     var grid_row = [];
+
+        //     for (var j = 0; j < this.size.x; j++) {
+        //         var cell_coords = {
+        //             x: this.spacing_x + j * (this.cell_width + this.spacing_x),
+        //             y: this.spacing_y + i * (this.cell_height + this.spacing_y)
+        //         }
+
+        //         grid_row.push(cell_coords);
+        //     }
+
+        //     this.grid_matrix.push(grid_row);
+        // }
+
+        for (var i = 0; i < this.size.x; i++) {
+            this.wall_map[i] = [];
+            this.grid_matrix[i] = [];
+
+            for (var j = 0; j < this.size.y; j++) {
+                this.wall_map[i][j] = 0;
+                this.grid_matrix[i][j] = {
+                    x: this.spacing_x + i * (this.cell_width + this.spacing_x),
+                    y: this.spacing_y + j * (this.cell_height + this.spacing_y)
+                };
+            }
+        }
+
+        this.objects = [];
+        this.drawPath = false;
+        this.updateGraphics();
+    }
 
     // GETTERS and SETTERS
-    getWidth() {
-        return this.canvas_obj.width;
-    }
-    setWidth(width_pixel) {
-        this.canvas_obj.width = width_pixel;
-    }
-    getHeight() {
-        return this.canvas_obj.height;
-    }
-    setHeight(height_pixel) {
-        this.canvas_obj.height = height_pixel;
-    }
-    getSize() {
-        return {
-            x: this.size_x,
-            y: this.size_y
-        }
-    }
     setSize(size_x, size_y) {
-        this.setSizeX(size_x);
-        this.setSizeY(size_y);
-    }
-    getSizeX() {
-        return this.size_x;
-    }
-    setSizeX(size_x) {
-        this.size_x = size_x;
-    }
-    getSizeY() {
-        return this.size_y;
-    }
-    setSizeY(size_y) {
-        this.size_y = size_y;
+        this.size.x = size_x;
+        this.size.y = size_y;
     }
     setSpacing(spacing_x, spacing_y) {
         this.spacing_x = spacing_x;
         this.spacing_y = spacing_y;
     }
-    setBackgroundColor(color) {
-        this.BG_COLOR = color;
+    setAlgorithm(algorithm) {
+        this.algorithm = algorithm;
     }
-    setCellsColor(color) {
-        this.CELL_COLOR = color;
-    }
-    printInfo() {
-        console.log(this.canvas_id);
-    }
+
 
 
     // DRAWING UTILS
@@ -142,7 +177,9 @@ class Grid {
     drawCells() {
         for (var i = 0; i < this.grid_matrix.length; i++) {
             for (var j = 0; j < this.grid_matrix[i].length; j++) {
+
                 var cell = this.grid_matrix[i][j];
+
                 this.context.fillStyle = this.CELL_COLOR;
                 this.context.fillRect(cell.x, cell.y, this.cell_width, this.cell_height);
             }
@@ -177,7 +214,7 @@ class Grid {
     }
 
     drawRect(obj) {
-        var cell = this.grid_matrix[obj.y][obj.x];
+        var cell = this.grid_matrix[obj.x][obj.y];        
 
         var size = {
             x: this.cell_width * obj.size,
@@ -191,10 +228,13 @@ class Grid {
 
         this.context.fillStyle = obj.color;
         this.context.fillRect(pos.x, pos.y, size.x, size.y);
+        this.context.lineWidth = 3;
+        this.context.strokeStyle = "white";
+        this.context.strokeRect(pos.x, pos.y, size.x, size.y);
     }
 
     drawCircle(obj) {
-        var cell = this.grid_matrix[obj.y][obj.x];
+        var cell = this.grid_matrix[obj.x][obj.y];
 
         var radius = this.cell_width < this.cell_height ?
             (this.cell_width / 2) * obj.size :
@@ -222,11 +262,9 @@ class Grid {
         this.context.strokeStyle = obj.color;
         this.context.lineWidth = (this.cell_height / 2) * obj.size;
         this.context.beginPath();
-        //this.context.strokeStyle = obj.color;
-        //this.context.lineWidth = (this.cell_height / 2) * obj.size;
 
         for (var i = 0; i < obj.pointList.length; i++) {
-            var cell = this.grid_matrix[obj.pointList[i].y][obj.pointList[i].x];
+            var cell = this.grid_matrix[obj.pointList[i].x][obj.pointList[i].y];
             var pos = {
                 x: cell.x + this.cell_width / 2,
                 y: cell.y + this.cell_height / 2
@@ -265,15 +303,15 @@ class Grid {
         }
 
 
-        if (type != this.LINE && cell_x >= this.size_x || cell_y >= this.size_y) {
-            console.error("point (" + cell_x + ", " + cell_y + ") outside of the grid.\n\tGrid size: ' + this.size_x + 'x' + this.size_y + '.");
+        if (type != this.LINE && cell_x >= this.size.x || cell_y >= this.size.y) {
+            console.error("point (" + cell_x + ", " + cell_y + ") outside of the grid.\n\tGrid size: ' + this.size.x + 'x' + this.size.y + '.");
             return;
         }
 
         if (type == this.LINE) {
             for (var i = 0; i < pointList.length; i++) {
-                if (pointList[i].x >= this.size_x || pointList[i].y >= this.size_y) {
-                    console.error("Point (" + pointList[i].x + ", " + pointList[i].y + ") in pointlist outside of the grid.\n\tGrid size: ' + this.size_x + 'x' + this.size_y + '.");
+                if (pointList[i].x >= this.size.x || pointList[i].y >= this.size.y) {
+                    console.error("Point (" + pointList[i].x + ", " + pointList[i].y + ") in pointlist outside of the grid.\n\tGrid size: ' + this.size.x + 'x' + this.size.y + '.");
                     return;
                 }
             }
@@ -287,7 +325,7 @@ class Grid {
             type: type,
             color: color,
             pointList: pointList
-        }
+        }        
 
         this.objects[name] = obj;
 
@@ -394,7 +432,7 @@ class Grid {
             }
 
             // Check validity of movement
-            if (obj.x < 0 || obj.x >= this.size_x || obj.y < 0 || obj.y >= this.size_y) {
+            if (obj.x < 0 || obj.x >= this.size.x || obj.y < 0 || obj.y >= this.size.y) {
                 console.error("Invalid new position.");
                 obj.x = old_x;
                 obj.y = old_y;
@@ -414,57 +452,15 @@ class Grid {
         this.updateGraphics();
     }
 
-    setAlgorithm(algorithm) {
-        this.algorithm = algorithm;
-    }
-
-
-    // Generate the grid based on setting specified before
-    generate() {
-        if (this.size_x == null || this.size_y == null) {
-            console.error("Missing size parameters.");
-            return false;
-        }
-
-        // Calculate size in pixel based on dimensions and number of cells
-        this.cell_width = ((this.canvas_obj.width - this.spacing_x) / this.size_x) - this.spacing_x;
-        this.cell_height = ((this.canvas_obj.height - this.spacing_y) / this.size_y) - this.spacing_y;
-
-        this.grid_matrix = [];
-        this.wall_map = [];
-
-        for (var i = 0; i < this.size_y; i++) {
-            this.wall_map[i] = [];
-
-            var grid_row = [];
-
-            for (var j = 0; j < this.size_x; j++) {
-                var cell_coords = {
-                    x: this.spacing_x + j * (this.cell_width + this.spacing_x),
-                    y: this.spacing_y + i * (this.cell_height + this.spacing_y)
-                }
-
-                grid_row.push(cell_coords);
-
-                this.wall_map[i][j] = 0;
-            }
-
-            this.grid_matrix.push(grid_row);
-        }
-
-        this.objects = [];
-        this.drawPath = false;
-        this.updateGraphics();
-    }
 
 
     // UTILS
     cellIsWall(x, y) {
-        if (x < 0 || y < 0 || x >= this.size_x || y >= this.size_y)
+        if (x < 0 || y < 0 || x >= this.size.x || y >= this.size.y)
             return true;
         else if (this.wall_map[x][y])
             return true;
-        /*else if ((x + 1 < this.size_x && y - 1 >= 0 && this.wall_map[x + 1][y] == 1 && this.wall_map[x][y - 1] == 1) || (x + 1 < this.size_x && y + 1 < this.size_y && this.wall_map[x + 1][y] == 1 && this.wall_map[x][y + 1] == 1) || (x - 1 >= 0 && y - 1 >= 0 && this.wall_map[x - 1][y] == 1 && this.wall_map[x][y - 1] == 1) || (x - 1 >= 0 && y + 1 < this.size_y && this.wall_map[x - 1][y] == 1 && this.wall_map[x][y + 1] == 1))
+        /*else if ((x + 1 < this.size.x && y - 1 >= 0 && this.wall_map[x + 1][y] == 1 && this.wall_map[x][y - 1] == 1) || (x + 1 < this.size.x && y + 1 < this.size.y && this.wall_map[x + 1][y] == 1 && this.wall_map[x][y + 1] == 1) || (x - 1 >= 0 && y - 1 >= 0 && this.wall_map[x - 1][y] == 1 && this.wall_map[x][y - 1] == 1) || (x - 1 >= 0 && y + 1 < this.size.y && this.wall_map[x - 1][y] == 1 && this.wall_map[x][y + 1] == 1))
             return true;
             */
         return false
@@ -478,11 +474,11 @@ class Grid {
         var cell_x = Math.ceil(x / (grid.cell_width + grid.spacing_x)) - 1;
         var cell_y = Math.ceil(y / (grid.cell_height + grid.spacing_y)) - 1;
 
-        if (cell_x < 0 || cell_x > this.size_x || cell_y < 0 || cell_y > this.size_y) {
+        if (cell_x < 0 || cell_x > this.size.x || cell_y < 0 || cell_y > this.size.y) {
             return null;
         }
 
-        // console.log("Cell ' + cell_x + ' - ' + cell_y);
+        // console.log("Cell " + cell_x + " - " + cell_y);
 
         return {
             x: cell_x,
@@ -586,7 +582,6 @@ class Grid {
         }
 
         grid.mouseWasDragged = false;
-
     }
 
     setOnCellClickDrag(evHandler) {
@@ -646,8 +641,8 @@ class Grid {
         var adjacency_matrix = {};
 
         // iterate over all cells
-        for (var i = 0; i < this.grid_matrix.length; i++) {
-            for (var j = 0; j < this.grid_matrix[i].length; j++) {
+        for (var i = 0; i < this.grid_matrix.length; i++) { // x
+            for (var j = 0; j < this.grid_matrix[i].length; j++) { // y
 
                 // console.log("Computing for cell (" + i + ", " + j + ").");
 
@@ -665,7 +660,7 @@ class Grid {
                             // console.log("\tChecking (" + k + ", " + l + ")...");
 
                             // check that target is inside the map
-                            if (k >= 0 && k < this.size_x && l >= 0 && l < this.size_y) {
+                            if (k >= 0 && k < this.size.x && l >= 0 && l < this.size.y) {
 
                                 // exclude the cell itself
                                 if (!(k == i && l == j)) {
@@ -746,9 +741,9 @@ class Grid {
 
         // Obstacle map, initialized to 0 in every cell
         this.obstacle_vertex_map = [];
-        for (var i = 0; i < this.size_x; i++) {
+        for (var i = 0; i < this.size.x; i++) {
             var l = []
-            for (var j = 0; j < this.size_y; j++) {
+            for (var j = 0; j < this.size.y; j++) {
                 l.push(0);
             }
             this.obstacle_vertex_map.push(l);
@@ -792,7 +787,7 @@ class Grid {
         single_paths.forEach(sp => {
             var ok = true;
             sp.path.forEach(step => {
-                if (this.wall_map[step.x][step.y] == 1) {
+                if (this.cellIsWall(step.x, step.y)) {
                     ok = false;
                     return;
                 }
@@ -892,10 +887,10 @@ class Grid {
 
         diagonalPositions.forEach(element => {
 
-            if (x + element[0] >= this.size_x || x + element[0] < 0 || y + element[1] >= this.size_y || y + element[1] < 0) {
+            if (x + element[0] >= this.size.x || x + element[0] < 0 || y + element[1] >= this.size.y || y + element[1] < 0) {
                 // Outside the map
                 return;
-            } else if (this.wall_map[x + element[0]][y] == 0 && this.wall_map[x][y + element[1]] == 0 && this.wall_map[x + element[0]][y + element[1]] == 0) {
+            } else if (!this.cellIsWall(x + element[0], y) && !this.cellIsWall(x, y + element[1]) && !this.cellIsWall(x + element[0], y + element[1])) {
                 if (this.obstacle_vertex_map[x + element[0]][y + element[1]] == 0) {
                     var obstacle_vertex_name = 'ov' + '_' + (x + element[0]) + '_' + (y + element[1]);
                     this.addCircle(obstacle_vertex_name, grid.MEDIUM, x + element[0], y + element[1], grid.OBSTACLE_EDGE_COLOR);
@@ -907,17 +902,17 @@ class Grid {
     }
 
     randomObstacleVertex() {
-        var SAMPLE_NUM = this.size_x;
-        var MAX_TRIES = this.size_x * 3;
+        var SAMPLE_NUM = this.size.x;
+        var MAX_TRIES = this.size.x * 3;
         var i = 0;
         var n = 0;
         var x, y;
 
         while (i < SAMPLE_NUM && n < MAX_TRIES) {
-            x = Math.floor(Math.random() * this.size_x);
-            y = Math.floor(Math.random() * this.size_y);
+            x = Math.floor(Math.random() * this.size.x);
+            y = Math.floor(Math.random() * this.size.y);
 
-            if (this.wall_map[x][y] == 0 && this.obstacle_vertex_map[x][y] == 0) {
+            if (!this.cellIsWall(x, y) && this.obstacle_vertex_map[x][y] == 0) {
                 this.obstacle_vertex_map[x][y] = 1;
                 var obstacle_vertex_name = "random_" + x + "_" + y;
                 this.addCircle(obstacle_vertex_name, grid.MEDIUM, x, y, grid.OBSTACLE_EDGE_COLOR);
@@ -1119,8 +1114,8 @@ class Grid {
 
     rangeArea(o, r) { //o is a point object, and r is the radio of the circonference ( in our case it will be a square, according to our distance definition, as the number of steps)
         var range = [];
-        for (var i = 0; i < this.size_y; i++) {
-            for (var j = 0; j < this.size_x; j++) {
+        for (var i = 0; i < this.size.y; i++) {
+            for (var j = 0; j < this.size.x; j++) {
                 if (o.x - r <= i && i <= o.x + r && o.y - r <= j && j <= o.y + r) {
                     range.push({
                         x: i,
